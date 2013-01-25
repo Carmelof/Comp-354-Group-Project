4071e918a857c2a47136354fdddd0b339ea26aa9
Addison Rodomista <addisonrodomista@gmail.com>
2013-01-25 13:53:48 -0500

    Got row numbers working

--- Main.java
+++ Main.java
@@ -1,42 +1,124 @@
-import javax.swing.JTable;
-import javax.swing.SwingUtilities;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JMenuBar;
-import javax.swing.JScrollPane;
+import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.print.PrinterException;
-
+import javax.swing.*;
+import javax.swing.UIManager.*;
+import javax.swing.event.*;
+import javax.swing.table.*;
+//-------------------------------
+// Source code acquired from 
+// http://stackoverflow.com/questions/8002445/trying-to-create-jtable-with-proper-row-header
+//-------------------------------
 public class Main {
 
-	
-	public static void main(String[] args) throws Exception {
-		//Initialize columns A-K
-		String[] columns = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"};
-		//Initialize The contents to 0
-		Object[][] content = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-    		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-    		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-    		, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }};
+    private JFrame frame = new JFrame("Comp 354-Excel Document");
+    private JScrollPane scrollPane;
+    private JTable table;
+    private DefaultTableModel model;
+    private TableRowSorter<TableModel> sorter;
+    private JTable headerTable;
 
-    JTable table = new JTable(content, columns);
-    JFrame frame = new JFrame();
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    JPanel jPanel = new JPanel(new GridLayout(2, 2));
-    jPanel.setOpaque(true);
-    table.setPreferredScrollableViewportSize(new Dimension(500, 500));
-    jPanel.add(new JScrollPane(table));
-    /* Add the panel to the JFrame */
-    frame.add(jPanel);
-    /* Display the JFrame window */
-    frame.pack();
-    frame.setVisible(true);
+    public Main() {
+        table = new JTable(10, 11);
+        for (int i = 0; i < table.getRowCount(); i++) {
+            table.setValueAt(i, i, 0);
+        }
+        sorter = new TableRowSorter<TableModel>(table.getModel());
+        table.setRowSorter(sorter);
+        model = new DefaultTableModel() {
 
-    table.print();
-  }
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public int getColumnCount() {
+                return 1;
+            }
+
+            @Override
+            public boolean isCellEditable(int row, int col) {
+                return false;
+            }
+
+            @Override
+            public int getRowCount() {
+                return table.getRowCount();
+            }
+
+            @Override
+            public Class<?> getColumnClass(int colNum) {
+                switch (colNum) {
+                    case 0:
+                        return String.class;
+                    default:
+                        return super.getColumnClass(colNum);
+                }
+            }
+        };
+        headerTable = new JTable(model);
+        for (int i = 0; i < table.getRowCount(); i++) {
+            headerTable.setValueAt("Row " + (i + 1), i, 0);
+        }
+        headerTable.setShowGrid(false);
+        headerTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        headerTable.setPreferredScrollableViewportSize(new Dimension(50, 0));
+        headerTable.getColumnModel().getColumn(0).setPreferredWidth(50);
+        headerTable.getColumnModel().getColumn(0).setCellRenderer(new TableCellRenderer() {
+
+            @Override
+            public Component getTableCellRendererComponent(JTable x, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+
+                boolean selected = table.getSelectionModel().isSelectedIndex(row);
+                Component component = table.getTableHeader().getDefaultRenderer().getTableCellRendererComponent(table, value, false, false, -1, -2);
+                ((JLabel) component).setHorizontalAlignment(SwingConstants.CENTER);
+                if (selected) {
+                    component.setFont(component.getFont().deriveFont(Font.BOLD));
+                    component.setForeground(Color.red);
+                } else {
+                    component.setFont(component.getFont().deriveFont(Font.PLAIN));
+                }
+                return component;
+            }
+        });
+        table.getRowSorter().addRowSorterListener(new RowSorterListener() {
+
+            @Override
+            public void sorterChanged(RowSorterEvent e) {
+                model.fireTableDataChanged();
+            }
+        });
+        table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                model.fireTableRowsUpdated(0, model.getRowCount() - 1);
+            }
+        });
+        scrollPane = new JScrollPane(table);
+        scrollPane.setRowHeaderView(headerTable);
+        table.setPreferredScrollableViewportSize(table.getPreferredSize());
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.add(scrollPane);
+        frame.pack();
+        frame.setLocation(150, 150);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        try {// UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if (info.getName().equals("Nimbus")) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            //e.printStackTrace();
+        }
+        EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                Main TestTableRowHeader = new Main();
+            }
+        });
+    }
 }
