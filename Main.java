26edb8bbae563935f8b36c41666e4da3d07fc75e
Simone Ovilma <simone.ovilma@gmail.com>
2013-01-30 22:48:38 -0500

    Added a command parser
    When a command is entered in the console, the JTable is updated to
    reflect the cells specified within the command. Keep in mind that to
    view the table you have to write "exit" (without the quotes) in the
    console. This will close the Scanner object and then display the updated
    table. I have yet to clean up the code (take out unused variables, group
    a list of lines within a method, etc.) I'll try to do it when I can. I
    added some comments and enclosed my code within 2 lines of comments.
    Obviously the parser isn't error proof so if you encounter any
    particular errors let me know.

--- Main.java
+++ Main.java
@@ -4,6 +4,15 @@ import javax.swing.*;
 import javax.swing.UIManager.*;
 import javax.swing.event.*;
 import javax.swing.table.*;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+//import java
 //-------------------------------
 // Source code acquired from 
 // http://stackoverflow.com/questions/8002445/trying-to-create-jtable-with-proper-row-header
@@ -22,8 +31,8 @@ public class Main {
         //Fill the table with empty values
         for (int i = 0; i < table.getRowCount(); i++) {
         	for (int j = 0; j < table.getColumnCount(); j++) {
-        		Cell cell = new Cell(0.0, i, j);
-        		table.setValueAt(cell, i, j);
+        		//Cell cell = new Cell(0.0, i, j); 
+        		table.setValueAt(0.0, i, j); //************** NEEDED TO BE CHANGED TO WORK (from "cell" to 0.0 ***********/
         	}
         }
         sorter = new TableRowSorter<TableModel>(table.getModel());
@@ -117,8 +126,148 @@ public class Main {
         frame.pack();
         frame.setLocation(150, 150);
         frame.setVisible(true);
+        
+      //*******************************************************my code (Simone)**************************************************//
+    	Scanner inputCommand = new Scanner(System.in);
+    	String inputStr = "";
+    	String noSpaceCommand = "";
+    	boolean quit = false;
+    	//Equation starts with a cell name followed by "=" i.e "A1="
+    	String startPattern = "^[A-Ka-k]\\d{1,2}=.+$";
+    	//Equation with numbers only i.e. (A1= 4.5), (A1= 5 + 6.7 - 8); NOT VALID => (A1= B1), (A1=B1*C4), (A1= B5 - 3.4)
+    	String numericPattern = "^[A-Ka-k]\\d{1,2}=[^A-Za-z=]*$";
+    	//Equation contains cell names i.e A1 = B1 + 4 - C1; DOESN'T APPLY TO => A1 = 4.7 + 90 (no cell names after "=");
+    	String alphaNumPattern = "^.*[A-K]\\d{1,2}.*$"; 
+    	
+    	while(!quit) {
+    	    		
+    		while(!noSpaceCommand.equals("EXIT")) {// As long as the input from the console isn't "exit" do...
+    			
+    			inputStr = inputCommand.nextLine();
+            	noSpaceCommand = inputStr.replaceAll("\\s", ""); //take out any whitespace chars
+            	noSpaceCommand = noSpaceCommand.toUpperCase(); //put the char in the cell name to uppercase i.e b1 => B1
+            	
+            	int row = 0;
+        		int column = 0;
+        		double cellValue = 0;
+        		
+            	if(noSpaceCommand.matches(startPattern)) {//if the command starts correctly i.e "A1=" continue
+            		
+            		//This will call the eval function from javascript to evaluate an arithmetic expression
+            		ScriptEngineManager manager = new ScriptEngineManager();
+            	    ScriptEngine engine = manager.getEngineByName("JavaScript");
+            	    String[] splitCommand = new String[2];
+        	    	String equation = "";
+        	    	String cellName = "";
+        	    	//split the equation in two halves i.e A1 = 4.5 + 30 => part1: A1, part2: 4.5 + 30
+        	    	splitCommand = noSpaceCommand.split("=");
+        	    	cellName = splitCommand[0]; //name of cell that will be modified
+        	    	equation = splitCommand[1]; //equation associated with the cell
+        	    	
+        	    	//if the equation only contains numbers
+            		if(noSpaceCommand.matches(numericPattern)) {
+                		
+            			row = getCellRow(cellName);
+                		column = getCellColumn(cellName);
+                		cellValue = 0;
+            			
+            			try {
+								Object result = engine.eval(equation); //evaluate the arithmetic expression
+								cellValue = (double) result;
+								
+								//store the value in the correct cell of the JTable
+								table.getModel().setValueAt(cellValue, getCellRow(cellName), getCellColumn(cellName));
+								
+            				} catch (ScriptException e) {
+							// TODO Auto-generated catch block
+            					System.out.println("The arithmetic equation is not valid");
+            					e.printStackTrace();
+            				}//end catch
+                	    
+                	}//end if
+                	
+            		//if the equation contains cell names
+                	else if(equation.matches(alphaNumPattern)) {
+                		//check if there are cell names after the "=" sign i.e check for C4 & F7 in "A1 = C4 + 5 - F7"
+                		Pattern MY_PATTERN = Pattern.compile("[A-K]\\d{1,2}");
+                		Matcher myMatch = MY_PATTERN.matcher(equation);
+                		                		
+                		String otherCells = "";
+                		String newEquation = equation;                 	
+            			
+                		//every time you find a cell name i.e A1, retrieve its index by using getCellRow & getCellColumn
+                		//if you find C4 its index is row = 4 & column = 3
+                		//once you find the index of the cell, retrieve its value and replace the name of the cell by its value
+                		//in the newEquation i.e if newEquation = "B1 + 4 + C2" and B1=7, C2=9; then newEquation becomes "7 + 4 + 9"
+                		while(myMatch.find()) {
+                			
+                			otherCells = myMatch.group();                			
+                			row = getCellRow(otherCells);
+                			column = getCellColumn(otherCells);
+                			cellValue = (double) table.getValueAt(row, column);
+                			                			
+                			newEquation = newEquation.replace(otherCells, Double.toString(cellValue));
+                	
+                		}
+                	                		
+                		try {
+							Object result = engine.eval(newEquation);								
+							table.getModel().setValueAt(result, getCellRow(cellName), getCellColumn(cellName));
+							
+        				} catch (ScriptException e) {
+						// TODO Auto-generated catch block
+        					System.out.println("The arithmetic equation is not valid");
+        					e.printStackTrace();
+        				}//end catch
+                		
+                	
+                	}
+            		
+                	else {
+                		System.out.println("The command is not valid");
+                	}
+                		
+                	
+            	}
+            	else {
+            		System.out.println("Exiting...");
+            		inputCommand.close();
+            	}
+            		
+    		}//end while
+    		
+    		if(noSpaceCommand.equals("EXIT"))
+    			quit = true;
+    		
+    	} //end while(!quit)
+    	
+        
     }
-
+    
+    //retrieves the cell row index from the cell name i.e. A1 => row index is 0 (1 - 1)
+    public int getCellRow (String cellName) {
+    	
+    	int row = 0;
+    	String[] tempArray = new String[2];
+    	
+    	tempArray = cellName.split("[A-K]");
+    	row = Integer.parseInt(tempArray[1]) - 1;
+    	
+    	return row;
+    }
+    
+    //retrieves the cell row index from the cell name i.e. A1 => column index is 0 (65 - 65); ASCII(A) = 65
+    public int getCellColumn (String cellName) {
+    	
+    	int column = 0;
+    	
+    	column = ( ((int) cellName.charAt(0)) - 65 );
+    	
+    	return column;
+    }
+    
+  //****************************************************my code (Simone)*****************************************************//
+    
     public static void main(String[] args) {
     	try {// UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
@@ -130,6 +279,9 @@ public class Main {
         } catch (Exception e) {
             //e.printStackTrace();
         }
+    	
+    	
+    	
         EventQueue.invokeLater(new Runnable() {
 
             @Override
